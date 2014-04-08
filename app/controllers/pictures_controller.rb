07f4f95f38718ca24e431a9dc2d1b5a8a62e427e
stasdimos <stas.dimos@icloud.com>
2014-04-08 17:43:07 -0400

    moving pictures in pictured controler

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,46 +1,10 @@
 class PicturesController < ApplicationController
   def index
-    @pictures = [
-      {
-        :title => "Chopping Wood.",
-        :artist => "Stas Dimos",
-        :url => "http://i.imgur.com/uLWAqih.jpg"
-      },
-      {
-        :title => "The Second Picture",
-        :artist => "Jimbo",
-        :url => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-      },
-      {
-        :title => "Into the Poppies",
-        :artist => "Jimbo 2",
-        :url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-      }
-    ]
+    @pictures = Picture.all
   end
 
-  def picture0
-    @picture = {
-        :title => "Chopping Wood.",
-        :artist => "Stas Dimos",
-        :url => "http://i.imgur.com/uLWAqih.jpg"
-      }
-  end
-
-  def picture1
-    @picture = {
-      :title => "The Second Picture",
-      :artist => "Jimbo",
-      :url => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"  
-    }
-  end
-
-  def picture2
-    @picture = {
-      :title => "Into the Poppies",
-      :artist => "Jimbo 2",
-      :url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-    }
+  def show
+   @picture = Picture.find(params[:id])
   end
 
   def new
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,25 @@
+Picture.create!(
+  :title => "Chopping Wood.",
+  :artist => "Stas Dimos",
+  :url => "http://i.imgur.com/uLWAqih.jpg"
+  )
+
+Picture.create!(
+  :title => "The Second Picture",
+  :artist => "Jimbo",
+  :url => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"  
+  )
+
+Picture.create!(
+  :title => "Into the Poppies",
+  :artist => "Jimbo 2",
+  :url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
+  )
+
+
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +27,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
