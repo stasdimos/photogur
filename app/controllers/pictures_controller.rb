6b5f3bba58b3a4daffa72ac95395a1f0ebae2f6b
stasdimos <stas.dimos@icloud.com>
2014-04-08 16:03:17 -0400

    Making dem pictures work.

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,6 +1,22 @@
 class PicturesController < ApplicationController
   def index
-    
+    @pictures = [
+      {
+        :title => "The Picture",
+        :artist => "Stas Dimos",
+        :url => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
+      },
+      {
+        :title => "The Second Picture",
+        :artist => "Jimbo",
+        :url => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
+      },
+      {
+        :title => "Into the Poppies",
+        :artist => "Jimbo 2",
+        :url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
+      }
+    ]
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Photogur::Application.routes.draw do
   get 'pictures' => 'pictures#index'
 end
 
-
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -58,4 +56,3 @@ end
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
