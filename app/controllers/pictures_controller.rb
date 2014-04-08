3a696a99629547179aac499a82664c49a770ced1
stasdimos <stas.dimos@icloud.com>
2014-04-08 16:52:46 -0400

    Making each picture show at it's own URL using static routes

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,9 +2,9 @@ class PicturesController < ApplicationController
   def index
     @pictures = [
       {
-        :title => "The Picture",
+        :title => "Chopping Wood.",
         :artist => "Stas Dimos",
-        :url => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
+        :url => "http://i.imgur.com/uLWAqih.jpg"
       },
       {
         :title => "The Second Picture",
@@ -18,5 +18,30 @@ class PicturesController < ApplicationController
       }
     ]
   end
+
+  def picture0
+    @picture = {
+        :title => "Chopping Wood.",
+        :artist => "Stas Dimos",
+        :url => "http://i.imgur.com/uLWAqih.jpg"
+      }
+  end
+
+  def picture1
+    @picture = {
+      :title => "The Second Picture",
+      :artist => "Jimbo",
+      :url => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"  
+    }
+  end
+
+  def picture2
+    @picture = {
+      :title => "Into the Poppies",
+      :artist => "Jimbo 2",
+      :url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
+    }
+  end
+
 end
 
