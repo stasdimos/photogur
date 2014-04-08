07e62c66b096b2cebb3e76b6a80539c835881599
stasdimos <stas.dimos@icloud.com>
2014-04-08 18:48:55 -0400

    final update

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,29 +1,26 @@
 class PicturesController < ApplicationController
-
   def index
     @pictures = Picture.all
   end
 
   def show
-   @picture = Picture.find(params[:id])
+    @picture = Picture.find(params[:id])
   end
-  
+
   def new
     @picture = Picture.new
   end
-  
-  def create  
+
+  def create
+    # make a new picture with what picture_params returns (which is a method we're calling)
     @picture = Picture.new(picture_params)
     if @picture.save
+      # if the save for the picture was successful, go to index.html.erb
       redirect_to pictures_url
     else
+      # otherwise render the view associated with the action :new (i.e. new.html.erb)
       render :new
     end
-  end
-
-  private
-  def picture_params
-    params.require(:picture).permit(:artist, :title, :url)
   end
 
   def edit
@@ -46,6 +43,8 @@ class PicturesController < ApplicationController
     redirect_to pictures_url
   end
 
+  private
+  def picture_params
+    params.require(:picture).permit(:artist, :title, :url)
+  end
 end
-
-
